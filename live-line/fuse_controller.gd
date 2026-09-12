b593b9386a784d767c40ea26b93a57abcd123d5f
MalteFP <maltefp@outlook.com>
2026-09-12 13:59:20 +0200

    added fuse texture

--- live-line/fuse_controller.gd
+++ live-line/fuse_controller.gd
@@ -2,7 +2,16 @@ extends Node2D
 
 var fusePos = []
 var firePos: Vector2
-
-func playerMoved(direction: Vector2):
-	fusePos.pop_back().queue_free()
+var lastDir := "up"
+func playerMoved(direction: String):
+	addNewFuse(direction)
+	
+func addNewFuse(dir: String):
+	var texture = "res://textures/sprites/Fuse/" + dir + lastDir + ".png"
+	var fuse = preload("res://fuse.tscn")
+	var instance = fuse.instantiate()
+	instance.global_position = get_parent().body.global_position
+	instance.get_node("Sprite2D").texture = load(texture)
+	add_child(instance)
+	lastDir = dir
 	
