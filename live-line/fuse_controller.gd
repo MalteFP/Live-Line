ae21599188590230e0b585282a5f399beec33fac
MalteFP <maltefp@outlook.com>
2026-09-12 14:12:15 +0200

    Fixed a crash in fuse

--- live-line/fuse_controller.gd
+++ live-line/fuse_controller.gd
@@ -1,10 +1,27 @@
 extends Node2D
 
-var fusePos = []
+var fuseArr = []
 var firePos: Vector2
 var lastDir := "up"
+
+var initFuseSize = 30
+
+func _ready():
+	await get_tree().create_timer(0).timeout
+	var texture = "res://textures/sprites/Fuse/upup.png"
+	for i in range(initFuseSize):
+		var fuse = preload("res://fuse.tscn")
+		var instance = fuse.instantiate()
+		instance.get_node("Sprite2D").texture = load(texture)
+		instance.global_position = get_parent().body.global_position + (initFuseSize - i) * Vector2(0,16)
+		fuseArr.append(instance)
+		add_child(instance)
+
 func playerMoved(direction: String):
 	addNewFuse(direction)
+	for i in range(2):
+		if fuseArr.size() >= 1:
+			fuseArr.pop_front().queue_free()
 	
 func addNewFuse(dir: String):
 	var texture = "res://textures/sprites/Fuse/" + dir + lastDir + ".png"
@@ -12,6 +29,6 @@ func addNewFuse(dir: String):
 	var instance = fuse.instantiate()
 	instance.global_position = get_parent().body.global_position
 	instance.get_node("Sprite2D").texture = load(texture)
+	fuseArr.append(instance)
 	add_child(instance)
 	lastDir = dir
-	
