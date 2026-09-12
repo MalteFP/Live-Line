d1509f18a1b8ff5d6727d9259c78583d809b1326
Saidyt <83219304+thederpblue@users.noreply.github.com>
2026-09-12 14:37:06 +0200

    changed names

--- live-line/HP.gd
+++ live-line/HP.gd
@@ -11,8 +11,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	get_node("CanvasLayer/Health").text = str(int(currentHealth)) + "/" + str(maxHealth)
-	get_node("CanvasLayer/Sprite2D").scale = Vector2(currentHealth/maxHealth*15,5)
-	get_node("CanvasLayer/Sprite2D").position = Vector2(currentHealth/maxHealth*15*16/2,40)
+	get_node("CanvasLayer/HealthBarText").text = str(int(currentHealth)) + "/" + str(maxHealth)
+	get_node("CanvasLayer/HealthBarBar").scale = Vector2(currentHealth/maxHealth*15,5)
+	get_node("CanvasLayer/HealthBarBar").position = Vector2(currentHealth/maxHealth*15*16/2,40)
 	await get_tree().create_timer(1.0).timeout 
 	currentHealth -= 1*delta
