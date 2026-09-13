8450db5aef50aaec82e9a28fae5be9bef05d3ab1
MalteFP <maltefp@outlook.com>
2026-09-13 09:12:09 +0200

    Added player animations

--- live-line/player.gd
+++ live-line/player.gd
@@ -25,36 +25,41 @@ func movement():
 	if Input.is_action_just_pressed("attack"):
 		pass
 	elif Input.is_action_just_pressed("up") and not is_point_inside(pos + Vector2(0,-16)):
+		sprite.play("walkingBack")
 		var tween = get_tree().create_tween()
 		if movementTween and movementTween.is_running():
 			movementTween.custom_step(1)
 		fuseController.playerMoved("up")
 		lastMove = "up"
-		tween.tween_property(body,"position", body.position + Vector2(0,-16), 0.1)
+		tween.tween_property(body,"position", body.position + Vector2(0,-16), 0.2)
 		movementTween = tween
 	elif Input.is_action_just_pressed("down") and not is_point_inside(pos + Vector2(0,16)):
+		sprite.play("walkingFront")
 		var tween = get_tree().create_tween()
 		if movementTween and movementTween.is_running():
 			movementTween.custom_step(1)
 		fuseController.playerMoved("down")
 		lastMove = "down"
-		tween.tween_property(body,"position", body.position + Vector2(0,16), 0.1)
+		tween.tween_property(body,"position", body.position + Vector2(0,16), 0.2)
 		movementTween = tween
 	elif Input.is_action_just_pressed("left") and not is_point_inside(pos + Vector2(-16,0)):
+		sprite.play("walkingSide")
 		var tween = get_tree().create_tween()
 		if movementTween and movementTween.is_running():
 			movementTween.custom_step(1)
 		fuseController.playerMoved("left")
 		lastMove = "left"
-		tween.tween_property(body,"position", body.position + Vector2(-16,0), 0.1)
+		tween.tween_property(body,"position", body.position + Vector2(-16,0), 0.2)
 		movementTween = tween
 	elif Input.is_action_just_pressed("right") and not is_point_inside(pos + Vector2(16,0)):
+		sprite.play("walkingSide")
+		body.scale = Vector2(-1,1)
 		var tween = get_tree().create_tween()
 		if movementTween and movementTween.is_running():
 			movementTween.custom_step(1)
 		fuseController.playerMoved("right")
 		lastMove = "right"
-		tween.tween_property(body,"position", body.position + Vector2(16,0), 0.1)
+		tween.tween_property(body,"position", body.position + Vector2(16,0), 0.2)
 		movementTween = tween
 	
 
@@ -67,16 +72,17 @@ func is_point_inside(point: Vector2) -> bool:
 	return result.size() > 0
 	
 func updateSprite():
-	if lastMove == "up":
-		body.scale = Vector2(1,1)
-		sprite.play("idleBack")
-	elif lastMove == "down":
-		body.scale = Vector2(1,1)
-		sprite.play("idleFront")
-	elif lastMove == "right":
-		body.scale = Vector2(-1,1)
-		sprite.play("idleSide")
-	elif lastMove == "left":
-		body.scale = Vector2(1,1)
-		sprite.play("idleSide")
+	if not movementTween or movementTween and not movementTween.is_running():
+		if lastMove == "up":
+			body.scale = Vector2(1,1)
+			sprite.play("idleBack")
+		elif lastMove == "down":
+			body.scale = Vector2(1,1)
+			sprite.play("idleFront")
+		elif lastMove == "right":
+			body.scale = Vector2(-1,1)
+			sprite.play("idleSide")
+		elif lastMove == "left":
+			body.scale = Vector2(1,1)
+			sprite.play("idleSide")
 	
