1f8ec007bc60f9a3ce3ab9892561c54aa3179629
Saidyt <83219304+thederpblue@users.noreply.github.com>
2026-09-14 10:07:10 +0200

    fixed loss screen

---
+++ live-line/scenes/loss_scene.gd
@@ -0,0 +1,22 @@
+extends Control
+
+func round_to_dec(num, digit):
+	return round(num * pow(10.0, digit)) / pow(10.0, digit)
+	
+func death() -> void:
+		
+	var minutes = str(round_to_dec(int(ScoreHolder.timeSpent/60),0)).split(".")[0]
+	var seconds = str(int(ScoreHolder.timeSpent)%60)
+	
+	
+	
+	$CanvasLayer.visible = true
+	$CanvasLayer/VBoxContainer/Fuse.text = "total fuse: " + str(int(ScoreHolder.totalFuse))
+	if !minutes == "0": 
+		if seconds.length() == 1:
+			seconds = "0" + seconds
+		$CanvasLayer/VBoxContainer/Time.text = "total time: " + minutes + " minutes and " + seconds + " seconds"
+	else :
+		$CanvasLayer/VBoxContainer/Time.text = "total time: " + seconds + " seconds"
+
+	$CanvasLayer/VBoxContainer/Score.text = "Score: " + str((ScoreHolder.totalFuse / ScoreHolder.timeSpent) * 1000)
